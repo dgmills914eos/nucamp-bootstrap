ac20287c90b363440b935a3075b2b39d63592517
Dan Mills <dgmills914eos@gmail.com>
2020-02-22 10:54:25 -0600

    week 4 assignment

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,14 @@
                 }
             });
         });
+        $(function() {
+            
+            $("#reserveButton").click(function(){
+                $("#reserveModal").modal("show");
+            });
+        });
+        $(function() {            
+            $("#loginButton").click(function(){
+                $("#loginModal").modal("show");
+            });
+        });
